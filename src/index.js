edbc787eb5aae773064835e0ce80357b3adb83e9
FRickReich <frickreich@gmail.com>
2022-03-30 10:20:02 +0200

    Add cors to server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) =>
 {
