5de45978a53d918ae07d754c7e2ebc3d56c23e8b
FRickReich <frickreich@gmail.com>
2022-03-31 16:24:42 +0200

    Add dotenv package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVICE_PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
